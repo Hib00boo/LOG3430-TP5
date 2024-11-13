55227c4dd21bafd1d518d184c994bf1172799210
Hiba Chaarani <hiba.chaarani@polymtl.ca>
2024-11-13 11:20:48 -0500

    First commit

---
+++ locustfile.py
@@ -0,0 +1,38 @@
+from locust import HttpUser, TaskSet, task, between
+
+class SimpleTasks(TaskSet):
+    @task(1)
+    def home(self):
+        # Test the home route
+        self.client.get("/")
+
+    @task(2)
+    def fast(self):
+        # Test the fast route
+        self.client.get("/fast")
+
+    @task(3)
+    def medium(self):
+        # Test the medium route
+        self.client.get("/medium")
+
+    @task(4)
+    def heavy(self):
+        # Test the heavy route
+        self.client.get("/heavy")
+
+    @task(1)
+    def echo(self):
+        # Test the echo route
+        payload = {"message": "Hello, Locust!"}
+        self.client.post("/echo", json=payload)
+
+class SimpleUser(HttpUser):
+    weight = 0.02
+    tasks = [SimpleTasks]
+    wait_time = between(1, 5)  # Simulate a wait time between requests
+
+class HeavyUser(HttpUser):
+    weight = 0.98
+    tasks = [SimpleTasks]
+    wait_time = between(5, 10)
